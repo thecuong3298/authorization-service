92722ee7c4403b44e56757b333ede3bca83f0e33
cuongdt <thecuong4298@gmail.com>
2022-07-16 21:01:48 +0700

    done

--- src/main/java/com/amilo/authservice/AuthServiceApplication.java
+++ src/main/java/com/amilo/authservice/AuthServiceApplication.java
@@ -3,9 +3,8 @@ package com.amilo.authservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
-@EnableAuthorizationServer
+@ComponentScan({"com.amilo.*", "com.common.*"})
 @SpringBootApplication
 public class AuthServiceApplication {
 
--- src/main/java/com/amilo/authservice/controller/TokenController.java
+++
@@ -1,2 +0,0 @@
-package com.amilo.authservice.controller;public class TokenController {
-}
